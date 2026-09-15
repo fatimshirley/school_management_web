a785897ea90e978b74d1bc214db730ca4867214b
fatimshirley <fatimshirley@gmail.com>
2026-09-15 12:52:47 +0000

    Add root redirect to login

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,22 +1,25 @@
 from django.contrib import admin
 from django.urls import include, path
 
-
 urlpatterns = [
-
     path(
-        'admin/',
-        admin.site.urls
-    ),
+    '',
+    lambda request: redirect('connexion'),
+    name='home'
+),
 
-    path(
-        '',
-        include('apps.accounts.urls')
-    ),
+path(
+    'admin/',
+    admin.site.urls
+),
 
-    path(
-        'dashboard/',
-        include('apps.dashboard.urls')
-    ),
+path(
+    '',
+    include('apps.accounts.urls')
+),
 
+path(
+    'dashboard/',
+    include('apps.dashboard.urls')
+),
 ]
